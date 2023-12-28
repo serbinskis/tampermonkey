a6d7847679cf26216adaa348af839bda137ce3ef
WobbyChip <66483092+WobbyChip@users.noreply.github.com>
2023-12-28 23:14:19 +0200

    Update CSDD Test.js

--- CSDD/CSDD Test.js
+++ CSDD/CSDD Test.js
@@ -9,12 +9,138 @@
 // @require      http://ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min.js
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=csdd.lv
 // @grant        GM_xmlhttpRequest
+// @grant        none
+// @require      https://www.example.com/some/js/GM_fetch.js
 // ==/UserScript==
 
-async function get(url) {
-    return await new Promise(resolve => {
-        GM_xmlhttpRequest ({ method: "GET", url: url, onload: resolve });
+async function loadCategory(cat, url) {
+    var category = JSON.parse((await (await fetch(url)).text()).replaceAll("\\\\", "\\"));
+    sessionStorage.setItem(`category_${cat}`, JSON.stringify(category));
+
+    for (const [key, value] of Object.entries(category)) {
+        localStorage.setItem(key, value);
+    }
+}
+
+async function loadCategories() {
+    await loadCategory("b", "https://raw.githubusercontent.com/WobbyChip/Tampermonkey/master/CSDD/B%20Category.txt");
+}
+
+function addButton(text, id, callback) {
+    var input = document.createElement("input");
+    input.className = "button csdd_test";
+    input.type = "button";
+    input.id = id;
+    input.value = text;
+    input.addEventListener("click", callback);
+    $("fieldset")[0].appendChild(input);
+}
+
+function loadQuestion(question) {
+    var answers = question.answers.map(e => e.answer).toString();
+    var key = hex_md5(question.media + question.question + answers);
+    var item = localStorage.getItem(key);
+    if (item) { console.log(JSON.parse(item)); }
+    return item ? JSON.parse(item) : null;
+}
+
+function saveQuestion(question) {
+    var answers = question.answers.map(e => e.answer).toString();
+    var key = hex_md5(question.media + question.question + answers);
+    localStorage.setItem(key, JSON.stringify(question));
+    console.log("Saved question.");
+    console.log(question);
+    console.log(localStorage.length);
+}
+
+function prepareQuestion(question) {
+    $('video').remove();
+    $('.block-label input').attr('checked', null);
+    $('.block-label').css('border-color', '');
+
+    if (question.media.includes('.mp4')) {
+        var video = document.createElement("video");
+        video.src = question.media;
+        video.controls = true;
+        $('.content-container')[0].prepend(video);
+    } else {
+        var image = `background-image:url('${question.media}'); background-size: contain; background-repeat: no-repeat; background-color: #f2f2f2; background-position: top;`;
+        $('.content-container')[0].setAttribute('style', image);
+        $('.content-container')[0].href = question.media;
+    }
+
+    $(".text")[0].textContent = question.question;
+    var dummy = $('.block-label').clone()[0];
+    $('.block-label').remove();
+
+    for (const answer of question.answers.sort(() => 0.5 - Math.random())) {
+        var answer_el = $(dummy).clone()[0];
+        answer_el.setAttribute('for', answer.answer);
+        answer_el.childNodes[1].id = answer.answer;
+        answer_el.childNodes[1].setAttribute('value', answer.answer.split('-').pop());
+        answer_el.childNodes[2].textContent = answer.text;
+        $('#atb > fieldset')[0].prepend(answer_el);
+    }
+}
+
+async function startCategoryTest(cat, imagesFirst) {
+    if (!sessionStorage[`category_${cat}`]) { return; }
+    window.addTime = () => {};
+    window.secondsTimeSpanToHMS = () => window.secondsTimeSpanToHMS_new(0);
+
+    window.secondsTimeSpanToHMS_new = (s) => {
+        var h = Math.floor(s / 3600);
+        var m = Math.floor((s % 3600) /60);
+        s = s % 60;
+
+        return (h < 10 ? '0' + h : h) + ":" +
+            (m < 10 ? '0' + m : m) + ":" +
+            (s < 10 ? '0' + s : s);
+    }
+
+    $('#header div.csn-time span').html(window.secondsTimeSpanToHMS_new(0));
+    $('#header div.csn-time').attr('val', 2);
+    //$('#header div.csn-time span').html('∞');
+
+    var timer = setInterval(() => {
+		var ttm = parseInt($('#header div.csn-time').attr('val'));
+		$('#header div.csn-time span').html(window.secondsTimeSpanToHMS_new(ttm));
+		$('#header div.csn-time').attr('val', ttm+1);
+    }, 1000);
+
+    var category = JSON.parse(sessionStorage.getItem(`category_${cat}`));
+    var questions = Object.entries(category).sort(() => 0.5 - Math.random());
+
+    var index = -1;
+    var cur_question = null;
+
+    $(".csdd_test").remove();
+    $('.controls-group').remove();
+
+    addButton("Pārbaudīt", "parbaudit", () => {
+        console.log(cur_question);
+        var answer = cur_question.answers.filter(e => e.correct)[0]?.answer;
+        console.log(answer);
+        $(`.block-label[for='${answer}']`)[0].click();
+        $(`.block-label[for='${answer}']`).css('border-color', 'green');
+        $(`.block-label[for!='${answer}']`).css('border-color', 'red');
     });
+
+    addButton("Izlaist", "izlaist", () => {
+        if (index+1 >= questions.length) { return clearInterval(timer); }
+        cur_question = JSON.parse(questions[index+++1][1]);
+        document.querySelector('.csn-question').childNodes[1].textContent = ` ${index+1}/${questions.length}`;
+        prepareQuestion(cur_question);
+    });
+
+    addButton("Atpakaļ", "atpakal", () => {
+        if (index-1 < 0) { return; }
+        cur_question = JSON.parse(questions[index---1][1]);
+        document.querySelector('.csn-question').childNodes[1].textContent = ` ${index+1}/${questions.length}`;
+        prepareQuestion(cur_question);
+    });
+
+    $(`#izlaist`).click();
 }
 
 (async function() {
@@ -22,44 +148,8 @@ async function get(url) {
         $(".ui-button.ui-corner-all.ui-widget.ui-button-icon-only.ui-dialog-titlebar-close")[0].click();
     }
 
-    if ($("#52")[0]) {
-        var url = "https://raw.githubusercontent.com/WobbyChip/Tampermonkey/master/CSDD/B%20Category.txt";
-        var category = JSON.parse((await get(url)).responseText.replaceAll("\\\\", "\\"));
-
-        for (const [key, value] of Object.entries(category)) {
-            localStorage.setItem(key, value);
-        }
-
-        return;
-    }
-
+    if ($("#52")[0]) { return await loadCategories(); }
     if (!$(".text")[0]) { return; }
-
-    function addButton(text, id, callback) {
-        var input = document.createElement("input");
-        input.className = "button";
-        input.id = id;
-        input.value = text;
-        input.addEventListener("click", callback);
-        $("fieldset")[0].appendChild(input);
-    }
-
-    function loadQuestion(question) {
-       var answers = question.answers.map(a => a.answer).toString();
-       var key = hex_md5(question.media + question.question + answers);
-       var item = localStorage.getItem(key);
-       if (item) { console.log(JSON.parse(item)); }
-       return item ? JSON.parse(item) : null;
-    }
-
-    function saveQuestion(question) {
-       var answers = question.answers.map(a => a.answer).toString();
-       var key = hex_md5(question.media + question.question + answers);
-       localStorage.setItem(key, JSON.stringify(question));
-       console.log("Saved question.");
-       console.log(question);
-       console.log(localStorage.length);
-    }
 
     var question = {
         media: $(".content-container a")[0]?.href || $(".content-container source")[0]?.src,
@@ -78,7 +168,8 @@ async function get(url) {
         saveQuestion(question);
     } else {
         var isStart = $(".csn-question")[0].textContent.includes("/3");
-        var isTest = $(".csn-time")[0].getAttribute("val") ? true : false;
+        var isFirst = $(".csn-question")[0].textContent.includes("1/");
+        var isTest = $(".csn-time")[0]?.getAttribute("val") ? true : false;
 
         if (isStart && !isTest) {
             $(".block-label[for='atbilde-2264']")[0]?.click();
@@ -87,19 +178,25 @@ async function get(url) {
             $(".button[type='submit']")[0].click();
         }
 
+        if (isFirst && isTest) {
+            addButton("SĀKT MEGA TESTU", "category-test", () => startCategoryTest('b', true));
+        }
+
         question = loadQuestion(question);
         if (!question) { return; }
 
         var answer = question.answers.filter(e => e.correct)[0]?.answer;
         if (!answer) { return; }
 
-        if (isTest) { addButton("Pārbaudīt", "parbaudit", () => {
-            $(`.block-label[for='${answer}']`)[0].click();
-        }); }
+        if (isTest) {
+            addButton("Pārbaudīt", "parbaudit", () => {
+                $(`.block-label[for='${answer}']`)[0].click();
+            });
 
-        if (isTest) { addButton("Izlaist", "izlaist", () => {
-            $(`.block-label[for='${answer}']`)[0]?.click();
-            $(".button[type='submit']")[0].click();
-        }); }
+            addButton("Izlaist", "izlaist", () => {
+                $(`.block-label[for='${answer}']`)[0]?.click();
+                $(".button[type='submit']")[0].click();
+            });
+        }
     }
 })();
