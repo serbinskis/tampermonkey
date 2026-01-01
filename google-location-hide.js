29674823960ea12370395136c1f91fbec97326a1
Adrian <66483092+serbinskis@users.noreply.github.com>
2026-01-01 05:46:46 +0200

    Update namespace in Google Location Hide script

--- google-location-hide.js
+++ google-location-hide.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Google Location Hide
-// @namespace    Serbix
+// @namespace    Serbinskis
 // @version      0.1
 // @description  Hide location when searching in google chrome
 // @author       Serbinskis
